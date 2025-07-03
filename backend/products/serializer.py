b72d2db5cad16f61bbc4775f393d51c97570c0e4
doyun-kim325 <rotla0325@naver.com>
2025-07-04 03:50:49 +0900

    아무거나:

--- backend/products/serializer.py
+++ backend/products/serializer.py
@@ -2,7 +2,7 @@
 from rest_framework import serializers
 from .models import Product
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer): #모델 <-> JSON 변환
     class Meta:
         model = Product
-        fields = '__all__'  # 모든 필드 
+        fields = '__all__'  # 모든 필드 포함
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializer import ProductSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
