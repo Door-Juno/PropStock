6e6f761cb37ff073129a52aaebd24f1888946447
doyun-kim325 <rotla0325@naver.com>
2025-07-06 23:24:31 +0900

    아무거나

--- backend/propstock/urls.py
+++ backend/propstock/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('products.urls')), #products 앱 URL을 프로젝트에 연결
 ]
+
+
