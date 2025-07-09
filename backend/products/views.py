d1c8377f5d6027821f5ea2bffc7ce50d16a325fb
doyun-kim325 <rotla0325@naver.com>
2025-07-09 16:56:15 +0900

    아무거나

--- backend/products/views.py
+++ backend/products/views.py
@@ -6,13 +6,13 @@ from .serializer import ProductSerializer
 from django.shortcuts import get_object_or_404
 
 class ProductListCreateAPIView(APIView):
-    def get(self, request):
+    def get(self, request): #품목 목록 확인
         search = request.query_params.get('search', '')
         products = Product.objects.filter(name__icontains=search)
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
+    def post(self, request): #품목 등록
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -20,15 +20,15 @@ class ProductListCreateAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProductDetailAPIView(APIView):
-    def get_object(self, pk):
-        return get_object_or_404(Product, pk=pk)
+    def get_object(self, pk): #DB에서 pk 기준으로 product 객체 가져옴
+        return get_object_or_404(Product, pk=pk) #해당 id 없을 시 error
 
     def get(self, request, pk):
         product = self.get_object(pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+        serializer = ProductSerializer(product) # 객체 -> 직렬화(JSON처럼 변환하기위해)
+        return Response(serializer.data) #직렬화된 데이터를 HTTP응답으로 보냄
 
-    def put(self, request, pk):
+    def put(self, request, pk): #전체 수정
         product = self.get_object(pk)
         serializer = ProductSerializer(product, data=request.data)
         if serializer.is_valid():
@@ -36,7 +36,7 @@ class ProductDetailAPIView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def patch(self, request, pk):
+    def patch(self, request, pk): #일부 수정
         product = self.get_object(pk)
         serializer = ProductSerializer(product, data=request.data, partial=True)
         if serializer.is_valid():
@@ -44,7 +44,7 @@ class ProductDetailAPIView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
+    def delete(self, request, pk): #품목 삭제
         product = self.get_object(pk)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
