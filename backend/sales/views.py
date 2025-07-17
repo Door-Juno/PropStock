ab223623fa471495cc4f497424d61d8a9c3eb297
doyun-kim325 <rotla0325@naver.com>
2025-07-17 17:21:59 +0900

    sales4

--- backend/sales/views.py
+++ backend/sales/views.py
@@ -15,28 +15,32 @@ class salesupload(APIView):
         if not uploaded_file:
             return Response({"message": "No file uploaded."}, status=status.HTTP_400_BAD_REQUEST)
 
-        try: #파일 읽는중 오류 발생 -> 중간에 멈추지 않게 하려고 예외 처리 시작!
+        try:
             if uploaded_file.name.endswith('.csv'):
                 df = pd.read_csv(uploaded_file)
             elif uploaded_file.name.endswith(('.xls', '.xlsx')):
                 df = pd.read_excel(uploaded_file)
             else:
                 return Response({"message": "Unsupported file format."}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e: # 파일 읽기 실페,손상,형식 오류 등이 나면 잡아내는 부분
+        except Exception as e:
             return Response({"message": f"Failed to read file: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
         
         total_rows = len(df)
         processed_rows = 0
         errors = []
+
+        item_codes = df['item_code'].unique() #전체 item_code 수집,unique() 중복 제거
+
+        existing_codes = set(
+            Product.objects.filter(item_code__in=item_codes).values_list('item_code', flat=True)
+        ) # 한 번의 쿼리로 DB에 있는 item_code만 가져오기
         
-        for idx, row in df.iterrows():
+        for idx, row in df.iterrows(): #반복문에서는 DB조회 대신 파이썬에서 검사
             try:
-                # 날짜 형식 확인(ex.2025-07-12)
                 pd.to_datetime(row['date'], format='%Y-%m-%d')
 
-                # 상품 코드가 DB에 있는지 확인
                 item_code = row['item_code']
-                if not Product.objects.filter(item_code=item_code).exists():
+                if item_code not in existing_codes:
                     raise ValueError(f"Item code '{item_code}' not found in database")
 
                 processed_rows += 1
@@ -54,4 +58,3 @@ class salesupload(APIView):
             "failed_rows": total_rows - processed_rows,
             "errors": errors
         }, status=status.HTTP_200_OK)
-
