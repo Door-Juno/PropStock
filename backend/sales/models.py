2a54c474c66a083f8cbf4d470a2f70ee3108888c
doyun-kim325 <rotla0325@naver.com>
2025-07-21 06:03:28 +0900

    sales5

--- backend/sales/models.py
+++ backend/sales/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+class Sales(models.Model):
+    date = models.DateField()  # row['date']
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)  # item_code를 이용해 Product 객체 조회 후 연결
+    quantity = models.IntegerField()  # row['quantity']
+    price = models.DecimalField(max_digits=11, decimal_places=2)  # row['price']
+
+    def __str__(self):
+        return f"{self.date} - {self.item} ({self.quantity})"
